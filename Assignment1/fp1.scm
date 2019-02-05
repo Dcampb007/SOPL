1d63f5f09f6c66ff54e7147e8750fb162643be39
Andre Campbell <campbellandre97@gmail.com>
2019-02-05 11:32:58 -0500

    Complete solution to problem 1. Added lines to reverse list

--- Assignment1/fp1.scm
+++ Assignment1/fp1.scm
@@ -2,12 +2,17 @@
 Andre Campbell
 Functional Programming 1
 |#
+
+;;; Problem 1
 (define (reverse-general L)
   (cond ((null? L) '()) ; if L is null, return empty list
         ((list? L)
-          ;;; handle reversal of list
+          ;;; append the first item to the end of the list
+          ;;; recursive call
+          (append (reverse-general (cdr L)) 
+              (list (reverse-general (car L))) 
+            )
         )
         (else L) ; else return L
     )
  )
-(display (reverse-general '()))
