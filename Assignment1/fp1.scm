5ba94592ba4b5dfb3a77c391373475423d2c625d
Andre Campbell <campbellandre97@gmail.com>
2019-02-06 16:36:43 -0500

    Start problem 2. Create skeleton for function

--- Assignment1/fp1.scm
+++ Assignment1/fp1.scm
@@ -16,3 +16,14 @@ Functional Programming 1
         (else L) ; else return L
     )
  )
+
+;;; Problem 2
+;;; This function expects a list as the parameter
+;;; if a list is not provided, it will return 0
+(define (sum-up-numbers-simple L)
+  (cond 
+    ((null? L ) 0) ; If the list is empty, return 0
+    (list? (helper (car L) (cdr L))) ; If L is a list, call helper
+    (else 0) ; Else return 0
+  )
+)
