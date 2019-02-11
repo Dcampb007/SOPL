0d1d14ffdccc6a98b7ca00da5560ddbe45529e5c
Andre Campbell <campbellandre97@gmail.com>
2019-02-10 19:50:14 -0500

    Adds the Helper2 function for problem3. Similar logic as Problem 2

--- Assignment1/fp1.scm
+++ Assignment1/fp1.scm
@@ -54,10 +54,33 @@ Functional Programming 1
 
 ;;; Problem 3
 ;;; This function is a modification of problem 2.
+
+; NOTE  
+; Since this code is very similar to the one above it,
+;I didn't break out the commits for this function
+(define (helper2 atm L)
+  (cond
+    ((and (null? L) (null? atm) 0)) ; If both are null, return 0
+    ((and (list? atm) (not_empty atm) (list? L) (not_empty L)) (+ (helper2 (car atm) (cdr atm)) (helper2 (car L) (cdr L))))
+    ((and (null? L) (list? atm) (not_empty atm)) (helper2 (car atm) (cdr atm))) 
+    ((and (list? atm) (list? L) (not_empty L)) (helper2 (car L) (cdr L))) ; If list is non empty, call helper
+    ((and (null? atm) (list? L) (not_empty L)) (helper2 (car L) (cdr L))) ; if list is non empty, call helper 
+
+    ((and (member atm alphabet) (not_empty L) (list? L)) (helper2 (car L) (cdr L))) ; if atm is in [a-z], call helper 
+    ((and (not (member atm alphabet)) (null? L)) atm) ; If atm not in [a-z] and L is null, return atm 
+    ((and (not (member atm alphabet)) (list? L)) (+ atm (helper2 (car L) (cdr L)))) ; Normal case
+
+   (else 0)
+  )
+)
+
 (define (sum-up-numbers-general L)
   (cond 
     ((null? L ) 0) ; If the list is empty, return 0
     ((list? L) (helper2 (car L) (cdr L))) ; If L is a list, call helper
     (else 0) ; Else return 0
   )
+)
+
+
 
