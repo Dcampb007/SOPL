e64c8ef5026f46aab9a2629bb06555ce52f8c24c
Andre Campbell <campbellandre97@gmail.com>
2019-02-19 17:32:47 -0500

    Skeleton commit for problem 2.

--- Assignment2/prolog.pl
+++ Assignment2/prolog.pl
@@ -25,3 +25,20 @@ sum-up-helper([H|T], N) :- % Else N is Res
 	\+number(H),
 	sum-up-helper(T, Res),
 	N is Res.
+
+% Problem 2
+% Write a predicate sum-up-numbers-general(L, N). L is a list, which may
+% contain as elements numbers and non-numbers. The predicate is true if N 
+% is the sum of all the numbers (including those in nested lists) in L. 
+% If there are no such numbers, the result is zero.
+sum-up-numbers-general(L, N) :- sum-up-helper(L, Y), Y=N.
+
+sum-up-helper2([],0). % Base case
+sum-up-helper2([H|T], N) :- % If H is a number, add it to Res
+	number(H),
+	sum-up-helper2(T, Res),
+	N is H + Res.
+sum-up-helper2([H|T], N) :- % Else N is Res
+	\+number(H),
+	sum-up-helper2(T, Res),
+	N is Res.
